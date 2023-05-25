feb506b935221a4fbcd0378d65f00cbf20012b48
superoverflow <cyrus.tang@superoverflow.com>
2023-05-25 21:20:23 +0100

    binance api failure

--- app/api/binance/route.js
+++ app/api/binance/route.js
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
-import { Spot } from "@binance/connector"
+// import { Spot } from "@binance/connector"
 
 export async function GET() {
-    const apiKey = process.env.BINANCE_API_KEY
-    const apiSecret =  process.env.BINANCE_API_SECRET
-    const client = new Spot(apiKey, apiSecret)
+    // const apiKey = process.env.BINANCE_API_KEY
+    // const apiSecret =  process.env.BINANCE_API_SECRET
+    // const client = new Spot(apiKey, apiSecret)
     
-    const response = await client.account()
-    const data = response.data
-    return NextResponse.json(data);
+    // const response = await client.account()
+    // const data = response.data
+    return NextResponse.json({"data" : "TODO"});
 }
