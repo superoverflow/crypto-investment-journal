b05f47039744723206fff68b22c11b08c4760fe0
superoverflow <cyrus.tang@superoverflow.com>
2023-05-27 21:33:25 +0100

    Fix build

--- app/api/binance/klines/route.js
+++ app/api/binance/klines/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(symbol) {
-    const code = symbol.toUpperCase()
-    const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${code}&interval=1d&limit=100`)
-    const json = await response.json()
+    // const code = symbol.toUpperCase()
+    // const response = await fetch(`https://api.binance.com/api/v3/klines?symbol=${code}&interval=1d&limit=100`)
+    // const json = await response.json()
     
-    return NextResponse.json({"data" : json});
+    return NextResponse.json({"data" : "json"});
 }
